6662f8bb9b3f81638d7dcf09e2db76d0d73dafdc
beachbliss <lukasbi1007@gmail.com>
2025-08-16 14:46:35 +0200

    adding daisy ui (hopefully soon)
    aaaaa

--- mbcme-website/tailwind.config.js
+++ mbcme-website/tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
      "./src/**/*.{js,jsx,ts,tsx}",
-     "./node_modules/flowbite-react/**/*.js",
-     "./node_modules/flowbite/**/*.js",
   ],
 theme: {
     extend: {},
-    plugins: [require("flowbite/plugin")],
+    plugins: [require('daisyui')],
+    daisyui: {
+      themes: ['nord'],
+    }
     },
   }
   
